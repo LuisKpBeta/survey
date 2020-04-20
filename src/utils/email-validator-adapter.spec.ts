36aaa959895df39ea7b0802ecac33242cc9f4e17
Luis Carlos Galvao <lu.ca.oliveira41@gmail.com>
2020-04-20 14:37:41 -0400

    test: ensure EmailValidatorAdapter returns false if validator return false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,8 @@
+import { EmailValidatorAdapter } from './EmailValidator'
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@email.com')
+    expect(isValid).toBe(false)
+  })
+})
