c6878f32e5a760fdb06360ef89cb7edc43341962
Luis Carlos Galvao <lu.ca.oliveira41@gmail.com>
2020-04-21 15:50:39 -0400

    test: ensure BcryptAdapter thow if bcrypt throws

--- src/infra/cryiptography/bcrypt-adapter.spec.ts
+++ src/infra/cryiptography/bcrypt-adapter.spec.ts
@@ -21,4 +21,12 @@ describe('Bcrypt adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
