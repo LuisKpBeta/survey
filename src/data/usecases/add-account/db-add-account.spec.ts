0226aede60392b3e424558b361a9a1104b7f690c
Luis Carlos Galvao <lu.ca.oliveira41@gmail.com>
2020-04-21 13:17:47 -0400

    refactor: group all protocols in a single file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,5 +1,5 @@
+import { Encrypter } from './db-add-account-protocols'
 import { DbAddAccount } from './db-add-account'
-import { Encrypter } from '../../protocols/encrypter'
 interface SutTypes {
   sut: DbAddAccount
   encrypterStub: Encrypter
