2de7448892c7345d3240e91fd007bc9d459253f8
Luis Carlos Galvao <lu.ca.oliveira41@gmail.com>
2020-04-21 13:42:33 -0400

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

---
+++ src/data/protocols/add-account-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../../domain/models'
+import { AddAccountModel } from '../../domain/usecases'
+export interface AddAccountRepository {
+  add (accountData: AddAccountModel): Promise<AccountModel>
+}
