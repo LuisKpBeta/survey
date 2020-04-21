f6cf8afaefae301258adf12921d892df41e29de5
Luis Carlos Galvao <lu.ca.oliveira41@gmail.com>
2020-04-21 17:36:21 -0400

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -13,5 +13,9 @@ export const MongoHelper = {
   },
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return { ...collectionWithoutId, id: _id }
   }
 }
