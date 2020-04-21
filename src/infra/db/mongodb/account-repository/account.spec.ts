693bc1aaf8cd9c30876cae27a50941466f7ee80d
Luis Carlos Galvao <lu.ca.oliveira41@gmail.com>
2020-04-21 17:29:56 -0400

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -4,6 +4,10 @@ describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
