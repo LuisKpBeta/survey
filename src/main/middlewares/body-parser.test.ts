4d7e0860cb8037182f3c14642697e05f33c37bc9
Luis Carlos Galvao <lu.ca.oliveira41@gmail.com>
2020-04-21 18:17:56 -0400

    test: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '../config/app'
+describe('Body parser Middleware', () => {
+  test('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Luis' })
+      .expect({ name: 'Luis' })
+  })
+})
