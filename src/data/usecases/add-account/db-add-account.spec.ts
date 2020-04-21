5dedb8eee4ce50815cf6e44c6e9dabc3aec37b72
Luis Carlos Galvao <lu.ca.oliveira41@gmail.com>
2020-04-21 13:55:02 -0400

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -90,4 +90,19 @@ describe('DBAddAccount UseCase', () => {
     const promisse = sut.add(accountData)
     await expect(promisse).rejects.toThrow()
   })
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
