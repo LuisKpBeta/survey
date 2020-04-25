eb810451387cbe8f6b61324d251983412c46757b
Luis Carlos Galvao <lu.ca.oliveira41@gmail.com>
2020-04-25 17:33:55 -0400

    refactor: make server return stack

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -32,7 +32,7 @@ export class SignUpController implements Controller {
       })
       return ok(account)
     } catch (error) {
-      return serverError()
+      return serverError(error)
     }
   }
 }
