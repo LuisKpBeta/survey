0d29c1957442013b3176f7a60cd15659b886b651
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-05-11 20:08:39 -0400

    test: ensure JwtAdapter throws if sign throws

--- src/infra/cryiptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryiptography/jwt-adapter/jwt-adapter.spec.ts
@@ -17,4 +17,12 @@ describe('JWT Adapter', () => {
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
