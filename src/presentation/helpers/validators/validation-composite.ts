78f083a874799ba219fd245c2ca09d07bade945e
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-05-17 16:36:12 -0400

    refactor: remove duplicated code

--- src/presentation/helpers/validators/validation-composite.ts
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -1,9 +1,6 @@
 import { Validation } from '../../protocols/validation'
 export class ValidationComposite implements Validation {
-  private readonly validations: Validation[]
-  constructor (validations: Validation[]) {
-    this.validations = validations
-  }
+  constructor (private readonly validations: Validation[]) {}
 
   validate (input: any): Error {
     for (const validation of this.validations) {
