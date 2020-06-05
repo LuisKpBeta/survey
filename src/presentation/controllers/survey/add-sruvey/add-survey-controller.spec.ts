43044d71aebaeee7b7eeb9398467f210b19ac31b
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-06-04 22:06:07 -0400

    test: ensure AddSurveyController returns 500 if AddSurveyUseCase throws

--- src/presentation/controllers/survey/add-sruvey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-sruvey/add-survey-controller.spec.ts
@@ -1,7 +1,7 @@
 import { HttpRequest, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
 import { Validation } from '../../../protocols'
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../../helpers/http/http-helper'
 const makeFakeRequest = (): HttpRequest => {
   return {
     body: {
@@ -61,5 +61,11 @@ describe('AddSurvey Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+  test('Should return 500 if AddSurvey throws', async () => {
+    const { sut, addSurveyStub } = makeSut()
+    jest.spyOn(addSurveyStub, 'add').mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
