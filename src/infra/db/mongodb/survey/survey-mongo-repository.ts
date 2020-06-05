ff153f07876476316e31f8045fc92f368b97c8fc
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-06-04 23:00:25 -0400

    feat: ensure SurveyMongoRepository add a new Survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -0,0 +1,9 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+export class SurveyMongoRepository implements AddSurveyRepository {
+  async add (surveyData: AddSurveyModel): Promise<void> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.insertOne(surveyData)
+  }
+}
