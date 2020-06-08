703d4f7488631c8a90547cd20c90659ba730dc07
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-06-08 14:03:03 -0400

    fix: change field name on test

--- src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -8,7 +8,7 @@ describe('Add Survey Validation factory', () => {
   test('Should call ValidationComposite with all validations', () => {
     makeAddSurveyValidation()
     const validations: Validation[] = []
-    for (const field of ['questions', 'answers']) {
+    for (const field of ['question', 'answers']) {
       validations.push(new RequiredFieldValidation(field))
     }
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
