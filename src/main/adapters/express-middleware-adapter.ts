5f824887c3179ac2edbc9bde086b2b876e42ec55
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-06-11 22:05:46 -0400

    feat: ensure add survey root returns 403 if no accessToken is provider

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,18 @@
+import { HttpRequest, Middleware } from '../../presentation/protocols'
+import { Request, Response, NextFunction } from 'express'
+export const adaptMiddleware = (middware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const httpResponse = await middware.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
