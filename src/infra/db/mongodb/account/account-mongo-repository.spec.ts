8c01fb95d70ea4c1fb03e8b3f80c193c801fc491
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-06-11 21:33:32 -0400

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -84,9 +84,25 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@email.com')
       expect(account.password).toBe('any_password')
     })
+    test('Should return an account on loadByToken success with role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@email.com')
+      expect(account.password).toBe('any_password')
+    })
     test.skip('Should return null if loadByToken fails', async () => {
       const sut = makeSut()
-      const account = await sut.loadByEmail('any_email@email.com')
+      const account = await sut.loadByToken('any_token')
       expect(account).toBeFalsy()
     })
   })
