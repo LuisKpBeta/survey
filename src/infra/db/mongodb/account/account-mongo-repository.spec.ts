737b279954bcc3d6b390911129f8517ae30d079b
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-06-11 21:29:53 -0400

    test: ensure account mongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -68,4 +68,26 @@ describe('Account Mongo Repository', () => {
       expect(account.accessToken).toBe('any_token')
     })
   })
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken success without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@email.com')
+      expect(account.password).toBe('any_password')
+    })
+    test.skip('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByEmail('any_email@email.com')
+      expect(account).toBeFalsy()
+    })
+  })
 })
