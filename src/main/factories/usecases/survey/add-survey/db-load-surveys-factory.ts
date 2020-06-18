ebb5221eef7d13828b370d52e9bdd5bf8ea48196
Luis Carlos <lu.ca.oliveira41@gmail.com>
2020-06-18 19:06:13 -0400

    feat: ensure load surveys route returns 403 if no access token is provided

---
+++ src/main/factories/usecases/survey/add-survey/db-load-surveys-factory.ts
@@ -0,0 +1,7 @@
+import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
+import DbLoadSurveys from '../../../../../data/usecases/load-surveys/db-load-surveys'
+
+export const makeDbLoadSurveys = (): DbLoadSurveys => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  return new DbLoadSurveys(surveyMongoRepository)
+}
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,7 +3,10 @@ import { adaptRoute } from '../adapters/express-routes-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { makeAuthMiddleware } from '../factories/middleware/auth-middleware-factory'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-survey-controller-factory'
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  const auth = adaptMiddleware(makeAuthMiddleware())
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
