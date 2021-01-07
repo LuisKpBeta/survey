787fd7854b1992cd450fe5f48bef92be68f2e8ae
Luis <lu.ca.oliveira41@gmail.com>
2021-01-07 16:50:12 -0400

    test: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -1,5 +1,7 @@
 export * from '@/presentation/protocols'
 export * from '@/domain/usecases/survey/load-survey-by-id'
+export * from '@/domain/usecases/survey-result/save-survey-result'
 export * from '@/domain/models/survey'
+export * from '@/domain/models/survey-result'
 export * from '@/presentation/errors'
 export * from '@/presentation/helpers/http/http-helper'
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,17 +1,23 @@
 import { serverError } from '../../survey/load-survey/load-survey-controller-protocols'
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById, forbidden, InvalidParamError } from './save-survey-result-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, forbidden, InvalidParamError, SaveSurveyResult } from './save-survey-result-controller-protocols'
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
       const { answer } = httpRequest.body
+      const accountId = httpRequest.accountId
       const survey = await this.loadSurveyById.loadById(surveyId)
       if (survey) {
         const index = survey.answers.findIndex(el => el.answer === answer)
         if (index === -1) {
           return forbidden(new InvalidParamError('survey answer'))
         }
+        await this.saveSurveyResult.save({ surveyId, accountId, date: new Date(), answers: answer })
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
