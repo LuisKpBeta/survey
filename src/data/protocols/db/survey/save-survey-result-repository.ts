fa133cc90bc6a213eff84498b02b4397b3e70d25
Luis <lu.ca.oliveira41@gmail.com>
2021-01-06 21:55:17 -0400

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/protocols/db/survey/save-survey-result-repository.ts
@@ -0,0 +1,5 @@
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
+export interface SaveSurveyResultRepository {
+  save (data: SaveSurveyResultModel): Promise<SurveyResultModel>
+}
