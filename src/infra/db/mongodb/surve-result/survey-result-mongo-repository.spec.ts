beb40b606f77a19250eeff62c5ea13bfa7b429cc
Luis <lu.ca.oliveira41@gmail.com>
2021-01-07 14:33:47 -0400

    test: ensure SurveyResultMongoRepository update resultif exists

--- src/infra/db/mongodb/surve-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/surve-result/survey-result-mongo-repository.spec.ts
@@ -59,5 +59,25 @@ describe('Account Mongo Repository', () => {
       expect(surveyResult.id).toBeTruthy()
       expect(surveyResult.answers).toBe(survey.answers[0].answer)
     })
+    test('Should update a survey result if its not new', async () => {
+      const sut = makeSut()
+      const survey = await makeSurvey()
+      const account = await makeAccount()
+      const res = await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId: account.id,
+        answers: survey.answers[1].answer,
+        date: new Date()
+      })
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answers: survey.answers[0].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toEqual(res.ops[0]._id)
+      expect(surveyResult.answers).toBe(survey.answers[0].answer)
+    })
   })
 })
