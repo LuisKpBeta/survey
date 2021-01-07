3957cf66ddd818d938065a5e13f2bd3d20bed4ce
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-01-07 16:55:49 -0400

    feat: ensure SaveSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,5 +1,5 @@
 import { serverError } from '../../survey/load-survey/load-survey-controller-protocols'
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById, forbidden, InvalidParamError, SaveSurveyResult } from './save-survey-result-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, forbidden, InvalidParamError, SaveSurveyResult, ok } from './save-survey-result-controller-protocols'
 export class SaveSurveyResultController implements Controller {
   constructor (
     private readonly loadSurveyById: LoadSurveyById,
@@ -17,11 +17,11 @@ export class SaveSurveyResultController implements Controller {
         if (index === -1) {
           return forbidden(new InvalidParamError('survey answer'))
         }
-        await this.saveSurveyResult.save({ surveyId, accountId, date: new Date(), answers: answer })
+        const surveyResult = await this.saveSurveyResult.save({ surveyId, accountId, date: new Date(), answers: answer })
+        return ok(surveyResult)
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
-      return null
     } catch (error) {
       return serverError(error)
     }
